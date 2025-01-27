5b35f8625f62c61366f7c57295ba908d8d2ca633
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2025-01-27 15:34:04 +0800

    init

---
+++ control.lua
@@ -0,0 +1,70 @@
+local function random_teleport(player)
+    local candidates = {}
+    local on_planet = false
+    for _, planet in pairs(game.planets) do
+        if planet.surface == nil then goto continue end -- if not visited
+        if player.surface == planet.surface then -- must on a planet, and cannot teleport to original planet
+            on_planet = true
+            goto continue
+        end
+        if planet.prototype.hidden then goto continue end -- deal with trench
+        if planet.surface.name == "nauvis" and
+            player.force.technologies["planet-discovery-nauvis"].researched ==
+            false then goto continue end -- deal with lignumis mod
+        candidates[#candidates + 1] = planet.name
+        ::continue::
+    end
+    if #candidates == 0 then
+        player.print({"random-teleport-no-planet"})
+        return
+    end
+    local surface_name = candidates[math.random(1, #candidates)]
+    local surface = game.surfaces[surface_name]
+    local position = surface.find_non_colliding_position("character", {0, 0},
+                                                         100, 1, true)
+    if position == nil then
+        player.print({"random-teleport-no-space", surface_name})
+        return
+    end
+    player.teleport(position, surface_name, true)
+    storage.last_teleport_time[player.name] = game.tick
+    game.print({"random-teleported", player.name, surface_name})
+end
+
+commands.add_command("random_teleport", {"random-teleport-help"},
+                     (function(command)
+    if command.player_index == nil then return end
+    local player = game.players[command.player_index]
+    local player_name = player.name
+    storage.last_teleport_time = storage.last_teleport_time or {}
+    storage.last_teleport_time[player_name] =
+        storage.last_teleport_time[player_name] or 0
+    local delta_time = game.tick - storage.last_teleport_time[player_name]
+    if delta_time < 60 * 60 * 30 and player.admin ~= true then
+        player.print({
+            "random-teleport-cooldown", 30, math.floor((delta_time) / 3600)
+        })
+        return
+    end
+    for _, inventory_id in pairs({
+        defines.inventory.character_main, defines.inventory.character_ammo,
+        defines.inventory.character_trash
+    }) do
+        local inventory = player.get_inventory(inventory_id)
+        if inventory == nil or inventory.is_empty() == false then
+            player.print({"random-teleport-inventory-not-empty"})
+            return
+        end
+    end
+    random_teleport(game.players[command.player_index])
+end))
+
+script.on_init(function() storage.last_teleport_time = {} end)
+
+script.on_event(defines.events.on_player_joined_game, (function(event)
+    local player = game.players[event.player_index]
+    if #game.players == 1 then return end -- skip the first player
+    if storage.last_teleport_time[player.name] then return end -- skip if already played once
+    random_teleport(player)
+    game.print({"server-welcome", player.name})
+end))
