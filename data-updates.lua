7a183e949f5e05fbb84566775098c61f70872b1f
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2025-02-05 08:56:42 +0800

    0.0.10

--- data-updates.lua
+++ data-updates.lua
@@ -1,48 +1,48 @@
-local tree_data = {
-    ["ashland-lichen-tree"] = {{name = "carbon", type = "item", amount = 2}},
-    ["ashland-lichen-tree-flaming"] = {
-        {amount = 2, name = "carbon", type = "item"}
-    },
-    ["cuttlepop"] = {
-        {amount = 8, name = "spoilage", type = "item"},
-        {amount = 2, name = "wood", type = "item"}
-    },
-    ["slipstack"] = {
-        {amount = 6, name = "spoilage", type = "item"},
-        {amount = 4, name = "stone", type = "item"}
-    },
-    ["funneltrunk"] = {
-        {amount = 4, name = "spoilage", type = "item"},
-        {amount = 6, name = "wood", type = "item"}
-    },
-    ["hairyclubnub"] = {
-        {amount = 5, name = "spoilage", type = "item"},
-        {amount = 5, name = "wood", type = "item"}
-    },
-    ["teflilly"] = {
-        {amount = 5, name = "spoilage", type = "item"},
-        {amount = 5, name = "wood", type = "item"}
-    },
-    ["lickmaw"] = {{amount = 10, name = "spoilage", type = "item"}},
-    ["stingfrond"] = {
-        {amount = 8, name = "spoilage", type = "item"},
-        {amount = 2, name = "wood", type = "item"}
-    },
-    ["boompuff"] = {
-        {amount = 6, name = "spoilage", type = "item"},
-        {amount = 4, name = "wood", type = "item"}
-    },
-    ["sunnycomb"] = {{amount = 10, name = "spoilage", type = "item"}},
-    ["water-cane"] = {{amount = 1, name = "wood", type = "item"}}
-}
+-- local tree_data = {
+--     ["ashland-lichen-tree"] = {{name = "carbon", type = "item", amount = 2}},
+--     ["ashland-lichen-tree-flaming"] = {
+--         {amount = 2, name = "carbon", type = "item"}
+--     },
+--     ["cuttlepop"] = {
+--         {amount = 8, name = "spoilage", type = "item"},
+--         {amount = 2, name = "wood", type = "item"}
+--     },
+--     ["slipstack"] = {
+--         {amount = 6, name = "spoilage", type = "item"},
+--         {amount = 4, name = "stone", type = "item"}
+--     },
+--     ["funneltrunk"] = {
+--         {amount = 4, name = "spoilage", type = "item"},
+--         {amount = 6, name = "wood", type = "item"}
+--     },
+--     ["hairyclubnub"] = {
+--         {amount = 5, name = "spoilage", type = "item"},
+--         {amount = 5, name = "wood", type = "item"}
+--     },
+--     ["teflilly"] = {
+--         {amount = 5, name = "spoilage", type = "item"},
+--         {amount = 5, name = "wood", type = "item"}
+--     },
+--     ["lickmaw"] = {{amount = 10, name = "spoilage", type = "item"}},
+--     ["stingfrond"] = {
+--         {amount = 8, name = "spoilage", type = "item"},
+--         {amount = 2, name = "wood", type = "item"}
+--     },
+--     ["boompuff"] = {
+--         {amount = 6, name = "spoilage", type = "item"},
+--         {amount = 4, name = "wood", type = "item"}
+--     },
+--     ["sunnycomb"] = {{amount = 10, name = "spoilage", type = "item"}},
+--     ["water-cane"] = {{amount = 1, name = "wood", type = "item"}}
+-- }
 
-for _, tree in pairs(data.raw.tree) do
-    if tree_data[tree.name] then
-        tree.minable.results = tree_data[tree.name]
-        tree.minable.count = nil
-        tree.minable.result = nil
-    end
-end
+-- for _, tree in pairs(data.raw.tree) do
+--     if tree_data[tree.name] then
+--         tree.minable.results = tree_data[tree.name]
+--         tree.minable.count = nil
+--         tree.minable.result = nil
+--     end
+-- end
 
 if data.raw["fluid"]["carbon-dioxide"] then
     data.raw["fluid"]["carbon-dioxide"].max_temperature = 3000
--- data.lua
+++ data.lua
@@ -1,12 +1,12 @@
-local lignumis_recipe_names = {
-    "low-density-structure-gold",
-    "rocket-fuel-from-wood-pulp-and-peat"
-}
+-- local lignumis_recipe_names = {
+--     "low-density-structure-gold",
+--     "rocket-fuel-from-wood-pulp-and-peat"
+-- }
 
-for _, recipe_name in pairs(lignumis_recipe_names) do
-    if data.raw.recipe[recipe_name] then
-        local not_recyclable_recipe = table.deepcopy(data.raw.recipe[recipe_name])
-        not_recyclable_recipe["auto_recycle"] = false
-        data:extend({not_recyclable_recipe})
-    end
-end
+-- for _, recipe_name in pairs(lignumis_recipe_names) do
+--     if data.raw.recipe[recipe_name] then
+--         local not_recyclable_recipe = table.deepcopy(data.raw.recipe[recipe_name])
+--         not_recyclable_recipe["auto_recycle"] = false
+--         data:extend({not_recyclable_recipe})
+--     end
+-- end
